77f8b1e64e9d27faaaac9c95bbfe8be4615defe7
Júlia de Castro <juliacastro@cpejr.com.br>
2022-06-03 15:18:50 -0300

    Atualizando testbench

--- alu/tb_alu.vhd
+++ alu/tb_alu.vhd
@@ -1,6 +1,7 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
 
 entity tb_alu is
 	generic
@@ -22,11 +23,13 @@ port (  CONTROL : in    std_logic_vector(3 downto 0);
 end component;
 
 SIGNAL A : std_logic_vector(3 downto 0);
-signal B, S, R: std_logic_vector	((DATA_WIDTH-1) downto 0);
+signal S1, S2, R: std_logic_vector	((DATA_WIDTH-1) downto 0);
 SIGNAL z : std_logic;
+
 begin
-instancia_alu: alu port map(CONTROL=>A,SRC1=>B,SRC2=>S,RESULT=>R,ZERO=>Z);
-A <= "0000";
-B <= "00000000000000000000000000000001";
-S <= "00000000000000000000000000000011";
+instancia_alu: alu port map(CONTROL=>A,SRC1=>S1,SRC2=>S2,RESULT=>R,ZERO=>Z);
+A <= "0000", "0001" after 30 ns, "0010" after 50 ns, "0110" after 70 ns, "0111" after 90 ns, "1100" after 110 ns;
+S1 <= "00000000000000000000000000010001", "00000000000000000000000000010111" after 50 ns;
+S2 <= "00000000000000000000000000000011";
+
 end teste;
